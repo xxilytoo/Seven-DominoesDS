c4b0e8b1b0055daacaa6cfd07bc7d55c1f936170
xxilytoo <107663344+xxilytoo@users.noreply.github.com>
2022-08-21 13:40:08 -0500

    Add files via upload

---
+++ Dominorunner.java
@@ -0,0 +1,14 @@
+public class Dominorunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DominoWindow Domino = new DominoWindow("Domino game");
+		
+		
+		
+		
+		
+		
+	}
+
+}
